b4bdc19f4ba404550adc1814e9ad9f0821ce8adf
vasim kadiwala <vasimkadiwala@gmail.com>
2016-08-15 22:12:01 +0530

    First Commit

---
+++ admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/jeans/admin/');
+define('HTTP_CATALOG', 'http://localhost/jeans/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/jeans/admin/');
+define('HTTPS_CATALOG', 'http://localhost/jeans/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/Jeans/admin/');
+define('DIR_SYSTEM', 'C:/wamp/www/Jeans/system/');
+define('DIR_IMAGE', 'C:/wamp/www/Jeans/image/');
+define('DIR_LANGUAGE', 'C:/wamp/www/Jeans/admin/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/Jeans/admin/view/template/');
+define('DIR_CONFIG', 'C:/wamp/www/Jeans/system/config/');
+define('DIR_CACHE', 'C:/wamp/www/Jeans/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/Jeans/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp/www/Jeans/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp/www/Jeans/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp/www/Jeans/system/storage/upload/');
+define('DIR_CATALOG', 'C:/wamp/www/Jeans/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'jeans');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://localhost/jeans/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://localhost/jeans/');
+
+// DIR
+define('DIR_APPLICATION', 'C:/wamp/www/Jeans/catalog/');
+define('DIR_SYSTEM', 'C:/wamp/www/Jeans/system/');
+define('DIR_IMAGE', 'C:/wamp/www/Jeans/image/');
+define('DIR_LANGUAGE', 'C:/wamp/www/Jeans/catalog/language/');
+define('DIR_TEMPLATE', 'C:/wamp/www/Jeans/catalog/view/theme/');
+define('DIR_CONFIG', 'C:/wamp/www/Jeans/system/config/');
+define('DIR_CACHE', 'C:/wamp/www/Jeans/system/storage/cache/');
+define('DIR_DOWNLOAD', 'C:/wamp/www/Jeans/system/storage/download/');
+define('DIR_LOGS', 'C:/wamp/www/Jeans/system/storage/logs/');
+define('DIR_MODIFICATION', 'C:/wamp/www/Jeans/system/storage/modification/');
+define('DIR_UPLOAD', 'C:/wamp/www/Jeans/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'jeans');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
